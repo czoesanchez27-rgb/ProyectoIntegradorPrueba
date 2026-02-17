fc9c332cc5647170aaac09c63106e74d402cdb76
Jonathan Ramírez <jonathanchepia@gmail.com>
2026-02-17 08:37:52 -0600

    controlador-y-modelo

--- ChatApp/src/java/org/integracode/chatapp/controller/CalcularReciclaje.java
+++ ChatApp/src/java/org/integracode/chatapp/controller/CalcularReciclaje.java
@@ -1,5 +1,29 @@
 package org.integracode.chatapp.controller;
 
+import org.integracode.chatapp.model.Reciclaje;
+
 public class CalcularReciclaje {
-    
+     public void calcular(Reciclaje r) {
+
+        double factor = 0;
+
+        switch (r.getMaterial()) {
+            case "Plástico":
+                factor = 1.5;
+                break;
+            case "Papel":
+                factor = 1.0;
+                break;
+            case "Vidrio":
+                factor = 2.0;
+                break;
+            case "Metal": 
+                factor = 2.5;
+                break;
+        }
+
+        double puntos = r.getCantidad() * factor;
+
+        r.setPuntos(puntos);
+    }
 }
--- ChatApp/src/java/org/integracode/chatapp/model/Reciclaje.java
+++ ChatApp/src/java/org/integracode/chatapp/model/Reciclaje.java
@@ -1,5 +1,32 @@
 package org.integracode.chatapp.model;
 
 public class Reciclaje {
-    
+
+    private String material;
+    private double cantidad;
+    private double puntos;
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(double puntos) {
+        this.puntos = puntos;
+    }
 }
